9f2773a4bf90e8f74e28b7862972e92e2bf02ed9
khalilpyh <khalilpyh@gmail.com>
2023-08-15 00:48:49 -0400

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,80 @@
+//general
+import logo from "./general/logo.svg";
+import backend from "./general/backend.png";
+import creator from "./general/creator.png";
+import mobile from "./general/mobile.png";
+import web from "./general/web.png";
+import github from "./general/github.png";
+import menu from "./general/menu.svg";
+import close from "./general/close.svg";
+
+//tech
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import csharp from "./tech/csharp.png";
+import azure from "./tech/azure.png";
+import dotnet from "./tech/dotnet.png";
+import mssql from "./tech/mssql.svg";
+import python from "./tech/python.png";
+import bootstrap from "./tech/bootstrap.png";
+
+//experiences
+import nc from "./company/nc.jpg";
+import ailiving from "./company/ailiving.jpg";
+
+//testimonials
+import soiloptix from "./company/soiloptix.png";
+
+//projects
+import goatrello from "./showcase/goatrello.png";
+import shareme from "./showcase/shareme.png";
+import ecommerce from "./showcase/ecommerce.png";
+import musiccollection from "./showcase/musiccollection.png";
+import esports from "./showcase/esports.png";
+import piggame from "./showcase/piggame.png";
+import simon from "./showcase/simon.png";
+import guessmynumber from "./showcase/guessmynumber.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  csharp,
+  azure,
+  dotnet,
+  mssql,
+  python,
+  bootstrap,
+  nc,
+  ailiving,
+  goatrello,
+  shareme,
+  ecommerce,
+  musiccollection,
+  esports,
+  piggame,
+  simon,
+  guessmynumber,
+  soiloptix,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,379 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  csharp,
+  azure,
+  dotnet,
+  mssql,
+  python,
+  bootstrap,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  nc,
+  ailiving,
+  goatrello,
+  shareme,
+  ecommerce,
+  musiccollection,
+  esports,
+  piggame,
+  simon,
+  guessmynumber,
+  soiloptix,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "tech",
+    title: "Tech Stack",
+  },
+  {
+    id: "project",
+    title: "Project",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Development",
+    icon: web,
+  },
+  {
+    title: "Back-end Developement",
+    icon: backend,
+  },
+  {
+    title: "Software Development",
+    icon: creator,
+  },
+  {
+    title: "Mobile App Developement",
+    icon: mobile,
+  },
+];
+
+const technologies = [
+  {
+    name: "C#",
+    icon: csharp,
+  },
+  {
+    name: ".NET",
+    icon: dotnet,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React.js",
+    icon: reactjs,
+  },
+  {
+    name: "Node.js",
+    icon: nodejs,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+  // {
+  //   name: "Tailwind CSS",
+  //   icon: tailwind,
+  // },
+  {
+    name: "Microsoft SQL Server",
+    icon: mssql,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "Git",
+    icon: git,
+  },
+  {
+    name: "Microsoft Azure",
+    icon: azure,
+  },
+  // {
+  //   name: "Figma",
+  //   icon: figma,
+  // },
+];
+
+const experiences = [
+  {
+    title: "Operations Coordinator",
+    company_name: "Ai Living Inc.",
+    icon: ailiving,
+    iconBg: "#383E56",
+    date: "September 2018 - October 2020",
+    points: [
+      "Planned and executed multiple interior design exhibitions at national and provincial levels.",
+      "Provided strategic direction to internal teams for meeting sales targets.",
+      "Conducted thorough financial performance reviews and reported findings to the general manager.",
+      "Implemented daily operational plans, including employee assignments and promotional strategies.",
+    ],
+  },
+  {
+    title: "Peer Tutor",
+    company_name: "Niagara College",
+    icon: nc,
+    iconBg: "#383E56",
+    date: "September 2022 - January 2023",
+    points: [
+      "Collaborated with students to complete homework assignments, identify areas for improvement, and address skill gaps.",
+      "Promoted effective study habits and learning skills.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "It was a pleasure to work with Yuhao in programming courses and entrepreneurship. He is an outstanding learner both practically and theoretically. Yuhao maintained high academic grades in my classes.Yuhao is trustworthy, organized, responsible, hardworking, detail-oriented, and demonstrates exceptional problem-solving capability.",
+    name: "Peter Vanscoy",
+    designation: "Professor",
+    company: "Niagara College",
+    image: nc,
+  },
+  {
+    testimonial:
+      "Congratulations on completing this chapter to move on to the next exciting one! All the best, and please, keep me posted as to what happens next for you 🙂!",
+    name: "Marsha Baddeley",
+    designation: "Professor",
+    company: "Niagara College",
+    image: nc,
+  },
+  {
+    testimonial:
+      "The website they made for us is very solid. We can't thank them enough!",
+    name: "Andrew Benton",
+    designation: "DevOps Manager",
+    company: "SoilOptix Inc.",
+    image: soiloptix,
+  },
+];
+
+const projects = [
+  {
+    name: "Goatrello",
+    description:
+      "Workflow tracking web application that allows users to search, track, and manage on-going projects, providing a convenient and efficient solution for client needs.",
+    tags: [
+      {
+        name: "ASP.NET Core",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Entity Framework",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "jQuery",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: goatrello,
+    source_code_link: "https://github.com/khalilpyh/Goatrello",
+  },
+  {
+    name: "ShareMe",
+    description:
+      "A photo-sharing social media web app with a responsive UI interface that allows people to share and download photos.",
+    tags: [
+      {
+        name: "React.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Tailwind CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Sanity",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "OAuth 2.0",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: shareme,
+    source_code_link: "https://github.com/khalilpyh/ShareMe",
+  },
+  {
+    name: "Ecommerce",
+    description:
+      "An e-commerce store of headphones allows customers to add items to carts and make the purchase through Stripe.",
+    tags: [
+      {
+        name: "Next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Sanity",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Stripe",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: ecommerce,
+    source_code_link: "https://github.com/khalilpyh/Ecommerce",
+  },
+  {
+    name: "Music Collection",
+    description:
+      "A progressive web app that manages musician, album, and song records with an integrated identity system.",
+    tags: [
+      {
+        name: "ASP.NET Core",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Entity Framework",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: musiccollection,
+    source_code_link: "https://mvcmusic20221206.azurewebsites.net/",
+  },
+  {
+    name: "Esports Manager",
+    description:
+      "A multi-platform E-sports management app that consumes game, team, and player data from a Web API.",
+    tags: [
+      {
+        name: ".NET MAUI",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "REST API",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Entity Framework",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "XML",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: esports,
+    source_code_link: "https://github.com/khalilpyh/EsportsManagement_MAUI",
+  },
+  {
+    name: "Pig Game(Dice Game)",
+    description:
+      "A dice game in which two players race to reach 100 points. Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player holds and scores the sum of the rolls.",
+    tags: [
+      {
+        name: "JavaScript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: piggame,
+    source_code_link: "https://github.com/khalilpyh/pig-game",
+  },
+  {
+    name: "Simon Game",
+    description:
+      "Classic simon game in which the bot creates a series of tones and lights and requires a user to repeat the sequence. ",
+    tags: [
+      {
+        name: "JavaScript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: simon,
+    source_code_link: "https://github.com/khalilpyh/simon-game",
+  },
+  {
+    name: "Guess My Number",
+    description: "Guess the number that is randomly generated.",
+    tags: [
+      {
+        name: "JavaScript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "HTML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: guessmynumber,
+    source_code_link: "https://github.com/khalilpyh/guess-my-number",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
